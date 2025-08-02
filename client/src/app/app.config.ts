722c8e64d144176bb2c17784cf888f988a17495f
AusBruce <wangyuheng086@gmail.com>
2025-08-02 14:21:52 +1000

    angular version conflict

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+
 import { provideForms } from '@angular/forms';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideClientHydration(), provideHttpClient(), provideForms()]
--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b061170e717c4c059c20bbc0180ae16d70613ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77f87108422a4f62128eef77fbee28f0cc16fbb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
