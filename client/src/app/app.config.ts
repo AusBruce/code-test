77f87108422a4f62128eef77fbee28f0cc16fbb2
AusBruce <wangyuheng086@gmail.com>
2025-08-02 12:46:35 +1000

    update to angular 18

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideForms } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration()]
+  providers: [provideClientHydration(), provideHttpClient(), provideForms()]
 };
